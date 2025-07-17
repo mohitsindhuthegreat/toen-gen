426773412cf8824d217ea2e122f7f10db67171cf
mohitsindhuthegreat <MOHITBHAIIND@GMAIL.COM>
2025-07-17 16:25:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import os
 # For Vercel/Netlify/Heroku deployment
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    debug = os.environ.get("FLASK_ENV") == "development"
+    debug = os.environ.get("FLASK_ENV") == "production"
     app.run(host="0.0.0.0", port=port, debug=debug)
